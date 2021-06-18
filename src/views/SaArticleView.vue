5f15105b92fdeeb43435d214fb4b475df2217ef4
Dimitri Steinbusch <dimitri.steinbusch@creatiview.be>
2021-06-18 18:20:04 +0200

    add article 15

--- src/views/SaArticleView.vue
+++ src/views/SaArticleView.vue
@@ -35,12 +35,13 @@ import article_11 from "@/views/articles/article_11.vue";
 import article_12 from "@/views/articles/article_12.vue";
 import article_13 from "@/views/articles/article_13.vue";
 import article_14 from "@/views/articles/article_14.vue";
+import article_15 from "@/views/articles/article_15.vue";
 import SaGoogleAdSense from "@/components/commons/SaGoogleAdSense.vue";
 
 export default defineComponent({
     components: {
         SaGoogleAdSense,
-        SaButton, article_1, article_2, article_3, article_4, article_5, article_6, article_7, article_8, article_9, article_10, article_11, article_12, article_13, article_14},
+        SaButton, article_1, article_2, article_3, article_4, article_5, article_6, article_7, article_8, article_9, article_10, article_11, article_12, article_13, article_14, article_15},
     setup(){
         const {params:{articleId}} = useRoute();
         const article = ref(new Article());
---
+++ src/views/articles/article_15.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="image">
+        <img :src="url1" alt="img article"/>
+    </div>
+    <div>
+        Estimant qu’il était important que sa ville devait être représentée à l’échelon du rap en Belgique. Fusko décida
+        de se lancer, ayant déjà quelques bases niveau rap dû à ses nombreux textes écrits, le fait de sortir son
+        premier son n’a pas été le plus compliqué.
+    </div>
+    <div>
+        <h2>Le Début du Succès :</h2>
+        <p>
+            Auteur de 2 sons à francs succès, « Libérable » qui est plus dans un thème de kickage et « Brasser » qui
+            quant à lui est plus dans une thématique de son d’été, réunissent à eux deux un peu plus de 160 milles vues.
+            Fusko est un de ces artistes capable de se diversifier et d’aller sur plusieurs terrains sans rencontrer
+            trop de problème.
+        </p>
+    </div>
+    <div>
+        <h2>Fusko sur les Réseaux :</h2>
+        <p>
+            Actuellement avec plus de 2500 abonnés sur instagram et plus de 3000 sur sa chaine youtube, Fusko a décidé
+            de lancer un projet de freestyle nommé « apparence trompeuse » qui en est déjà à l’épisode 2. Il nous confie
+            également qu’au courant de cet été il aimerait enchainer les clips et les sons.
+        </p>
+        <p>
+            Il est reconnaissant et gratifié de toutes les personnes qui s’abonnent pour suivre son travail.
+        </p>
+    </div>
+    <div>
+        <h2>Conclusion :</h2>
+        <p>
+            Dans le rap il veut transmettre son vécu, ses valeurs,.. Artiste humble, il ne manque pas de nous dire,
+            qu’il remercie vraiment ses proches pour tous les encouragements et toute la force qu’ils donnent.
+        </p>
+    </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue"
+import {useRoute} from "vue-router";
+import {urlPicture} from "@/utils/urls";
+
+export default defineComponent({
+    setup() {
+        const {params: {articleId}} = useRoute();
+        let id: number | undefined = undefined;
+        if (typeof articleId === 'string')
+            id = parseInt(articleId);
+
+        const url1 = urlPicture(id, 1);
+
+        return {
+            url1
+        }
+    }
+})
+</script>
+
+<style scoped>
+div {
+    margin-bottom: 50px;
+}
+
+.image {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.image img {
+    max-width: 100%;
+}
+</style>
